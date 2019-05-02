4c1804dfc4413f71d6f7dad200ccb1afcb5aefd8
Jade Dickinson <jadedickinson@googlemail.com>
2019-05-02 12:33:08 +0100

    Following along with Harvard CS50M mobile development with React Native MOOC

---
+++ code-examples/week1/syntax.js
@@ -0,0 +1,3 @@
+const firstName = "Jade":
+const lastName = 'Dickinson'
+// string literal - in JS either single quotes or double quotes are fine. Also the semi-colon at the end of an expression is optional.
