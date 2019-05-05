503bbddac6658ca16ea8bffebadfadabe02675b8
Jade Dickinson <jadedickinson@googlemail.com>
2019-05-05 03:14:34 +0100

    Add notes and example methods

--- code-examples/week3/MySet.js
+++ code-examples/week3/MySet.js
@@ -2,11 +2,25 @@
 // Set class and add some stuff to it:
 class MySet extends Set {
   constructor(arr) {
-    super(arr)
-    this.originalArray = arr
+    // Super keyword allows us to refer to the original class:
+    super(arr) // run the original set's constructor
+    this.originalArray = arr // this.originalArray is the array (arr) passed in as a parameter
   }
 
   add(val) {
     super.add(val)
+    // super refers to the X, so when
+    // we use super.add it goes ahead and
+    // uses the native implementation of the add() method
+    console.log(`added ${val} to the set!`)
+    // interpolation: notice the use of backtick-quoted string aka template literals
+  }
+
+  toArray() {
+    return Array.from(this)
+  }
+
+  reset() {
+    return new MySet(this.originalArray)
   }
 }
