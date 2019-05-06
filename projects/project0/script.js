8e1b909b9201c11a65a27da8c6c051cf730d95fc
Jade Dickinson <jadedickinson@googlemail.com>
2019-05-06 10:10:45 +0100

    Add comments

--- projects/project0/script.js
+++ projects/project0/script.js
@@ -25,4 +25,10 @@ function newTodo() {
   countElement.innerHTML = currentCount
   // TODO: Making it pretty - add checkbox to each el
   // TODO: Implement uncheckedCount and figure out how it's meant to be used.
+  // Note: this is taking longer than I expected but it's quite different to the frontend I do at work.
+  // Note: I'm impatient to get onto the React Native project.
+  // Going to skip this bit and go onto project1 for React Native.
+  // I'll come back if the React Native stuff seems too much or there are stumbling blocks.
+  // Maybe ask for a code review e.g. in JS would this method be separated out into functions, 
+  // is there a better approach to workflow than just docs and hacking through?
 }
